f9760a25d6c645ee978433c4099bf1a77e35915e
MINIYI9 <oluwagbeminiyi09@gmail.com>
2024-11-20 09:06:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let header= document.getElementById("sechead")
+
+let paragraph= document.getElementById("secparagraph")
+
+
+
+function firstbtn(){
+    header.innerText= "Manufactured with the best materials"
+    paragraph.innerText= "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+}
